ccc2f34fac3b80b2327072edae040d2406a96ad9
Evans-Junior <kwakukumi14@gmail.com>
2025-11-02 01:46:35 +0000

    working on all network4

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -20,7 +21,7 @@ app.add_middleware(
 )
 
 
-@app.get("/")
+@app.api_route("/", methods=["GET", "HEAD"])
 def root():
     return {"message": "Welcome to the Ghana Mall API"}
 
@@ -58,4 +59,5 @@ def get_all_ranks():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
